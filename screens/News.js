1d75d480ea59ac919b911455a2cce150e8cd6488
im-noob <aaravonly4you@gmail.com>
2019-01-01 13:21:08 +0530

    now ready for 20 add with news

--- screens/News.js
+++ screens/News.js
@@ -3,10 +3,20 @@ import {
     StyleSheet,
     WebView ,
     View,
+    Platform
 } from "react-native";
 import { Container, Spinner, Button, Text,Content} from 'native-base';
 import {createDrawerNavigator,DrawerItems, SafeAreaView,createStackNavigator,NavigationActions } from 'react-navigation';
 import Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Constants, Notifications, Permissions } from 'expo';
+async function getiOSNotificationPermission() {
+    const { status } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    if (status !== 'granted') {
+      await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    }
+  }
 
 export default class NewsScreen extends Component {
     constructor(props){
@@ -17,7 +27,39 @@ export default class NewsScreen extends Component {
     }
     componentDidMount() {
         setTimeout(() => {this.setState({renderCoponentFlag: true})}, 0);
+        getiOSNotificationPermission();
+        this.listenForNotifications();
+        this._handleButtonPress();
     }
+    _handleButtonPress = () => {
+        const localnotification = {
+          title: 'Cuteii Click me!',
+          body: 'Hello Bandar just do this',
+          android: {
+            sound: true,
+          },
+          ios: {
+            sound: true,
+          },
+        };
+        let sendAfterFiveSeconds = Date.now();
+        sendAfterFiveSeconds += 1000;
+    
+        const schedulingOptions = { repeat :'day' };
+        Notifications.scheduleLocalNotificationAsync(
+          localnotification,
+          schedulingOptions
+        );
+      };
+      listenForNotifications = () => {
+        Notifications.addListener(notification => {
+          if (notification.origin === 'received' && Platform.OS === 'android') {
+            alert("Chalo ab kar lo issko!!! warna bhul jaoge");
+          }
+        });
+      };
+
+
 
     render() {
         const {renderCoponentFlag} = this.state;
@@ -27,7 +69,10 @@ export default class NewsScreen extends Component {
                     <WebView
                         source={{uri: 'https://mycuteiii.blogspot.com/'}}
                         style={{marginTop: 20}}
-                        onLoad={alert("Check The Post Whats New For you!!!")}                        
+                        onLoad={
+                            alert("Check The Post Whats New For you!!!")
+                           
+                            }                        
                         />
                 </Container>
             );
